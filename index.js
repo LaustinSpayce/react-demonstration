f95203c3f3fdeb8261f1723ea2a3bf545331c7d6
Stuart Myers <stuart.myers@gmail.com>
2020-04-09 17:02:53 +0800

    Fix root directory

--- index.js
+++ index.js
@@ -11,12 +11,6 @@ app.set('views', __dirname + '/views');
 // this line sets react to be the default view engine
 app.set('view engine', 'jsx');
 
-app.get('/', (req, res) => {
-  // running this will let express to run home.handlebars file in your views folder
-  const data = { name: "GA class SEI-22"}
-  res.render('home', data)
-})
-
 // Create an array of Pokemon.
 const pokemon = [
   { name: 'pikachu',
@@ -32,7 +26,7 @@ const pokemon = [
   cute: true
   },
   { name: 'Sandshrew',
-  type: ['ground',],
+  type: ['ground'],
   cute: false 
   },
   { name: 'Vulpix',
@@ -52,6 +46,12 @@ app.get('/:name', (req, res) => {
   }
   // Tell the renderer the view file we want to use
   // and the data for the view file.
+  res.render('home', data)
+})
+
+app.get('/', (req, res) => {
+  // running this will let express to run home.handlebars file in your views folder
+  const data = { name: "GA class SEI-22", pokemon: pokemon }
   res.render('home', data)
 })
 
